7a31ac1637e65827c40654bd76d36aa95168c359
github username <chao.alexander.123@gmail.com>
2018-11-06 16:40:08 -0500

    create tables, models and controllers with the views

---
+++ db/migrate/20181106205555_doctors.rb
@@ -0,0 +1,9 @@
+class Doctors < ActiveRecord::Migration
+  def change
+    create_table :doctors do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20181106205603_patients.rb
@@ -0,0 +1,9 @@
+class Patients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
